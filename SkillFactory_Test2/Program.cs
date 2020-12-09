03132c7128f9415b3046b55ca5c828157b7f81b6
Andrew <konoplyanik0504@gmail.com>
2020-12-09 14:33:59 +0300

    Add project files.

---
+++ SkillFactory_Test2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SkillFactory_Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Andrew";
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
