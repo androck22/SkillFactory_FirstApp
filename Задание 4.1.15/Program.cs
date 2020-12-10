9d4166b15af59f6e1f7931a5250ceb2793b83b05
Andrew <konoplyanik0504@gmail.com>
2020-12-10 23:41:03 +0300

    Задание 4.1.15

---
+++ Задание 4.1.15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Задание_4._1._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском языке с маленькой буквы");
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("You color is cyan!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
